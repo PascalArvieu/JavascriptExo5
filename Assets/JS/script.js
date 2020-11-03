74df6580c41a4e924dd03b945aa767ab8184970b
PascalArvieu <pascalarvieu@gmail.com>
2020-11-03 12:16:30 +0100

    repush de l'exercice 5 JS

---
+++ Assets/JS/script.js
@@ -0,0 +1,11 @@
+
+// Exercice 5 
+
+function keskiReste() {
+    var dividende = document.getElementById("valeurX").value;
+    var diviseur = document.getElementById("valeurY").value;
+    var resultat = dividende % diviseur;
+
+    alert("le reste de " + dividende + " divisé par " + diviseur + " est : " + resultat) 
+}
+
